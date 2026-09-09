ba15b4780767188e2e720b48de8f389c6eb7747b
qaravparlaq-afk <qaravparlaq@gmail.com>
2026-09-09 04:13:08 +0330

    feat: add Android app module

---
+++ apps/android/app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "org.nexa.messenger"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "org.nexa.messenger"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.material3:material3:1.3.1")
+}

64ff3cefcf5f87d2610aa63c647b258256d4fc36
qaravparlaq-afk <qaravparlaq@gmail.com>
2026-09-09 04:13:11 +0330

    feat: add Android Compose entry screen

---
+++ apps/android/app/src/main/java/org/nexa/messenger/MainActivity.kt
@@ -0,0 +1,30 @@
+package org.nexa.messenger
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("NEXA")
+                    Text("Private. Open. E2EE by design.")
+                }
+            }
+        }
+    }
+}
